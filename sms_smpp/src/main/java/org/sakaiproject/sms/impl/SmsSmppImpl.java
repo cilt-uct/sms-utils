85715e9fab4a276ded0670e8c562a09f25c37a4e
Etienne <etienne@psybergate.co.za>
2008-11-21 12:00:00 +0000

    reformat

--- sms_smpp/src/main/java/org/sakaiproject/sms/impl/SmsSmppImpl.java
+++ sms_smpp/src/main/java/org/sakaiproject/sms/impl/SmsSmppImpl.java
@@ -58,6 +58,35 @@ import org.sakaiproject.sms.hibernate.model.SmsMessage;
 import org.sakaiproject.sms.model.SmsDeliveryReport;
 
 public class SmsSmppImpl implements SmsSmpp {
+
+	private final static org.apache.log4j.Logger LOG = org.apache.log4j.Logger
+			.getLogger(SmsSmppImpl.class);
+	private static TimeFormatter timeFormatter = new AbsoluteTimeFormatter();
+	private BindThread bindTest;
+	private int bindThreadTimer;
+	private byte dataCoding;
+	private ArrayList<SmsDeliveryReport> delReports = new ArrayList<SmsDeliveryReport>();
+	private byte destAddressNPI;
+	private byte destAddressTON;
+	private boolean disconnectGateWayCalled;
+	private int enquireLinkTimeOut;
+	private String gatewayAdress;
+	private boolean gatewayBound = false;
+	private String password;
+	private int port;
+	private byte priorityFlag;
+	private Properties properties = new Properties();
+	private byte protocolId;
+	private byte replaceIfPresentFlag;
+	private String serviceType;
+	private SMPPSession session = new SMPPSession();
+	private byte smDefaultMsgId;
+	private String sourceAddress;
+	private byte sourceAddressNPI;
+	private byte sourceAddressTON;
+	private String systemType;
+	private String userName;
+
 	class BindThread implements Runnable {
 
 		boolean allDone = false;
@@ -141,36 +170,6 @@ public class SmsSmppImpl implements SmsSmpp {
 			}
 		}
 	}
-
-	private final static org.apache.log4j.Logger LOG = org.apache.log4j.Logger
-			.getLogger(SmsSmppImpl.class);
-
-	private static TimeFormatter timeFormatter = new AbsoluteTimeFormatter();
-
-	private BindThread bindTest;
-	private int bindThreadTimer;
-	private byte dataCoding;
-	private ArrayList<SmsDeliveryReport> delReports = new ArrayList<SmsDeliveryReport>();
-	private byte destAddressNPI;
-	private byte destAddressTON;
-	private boolean disconnectGateWayCalled;
-	private int enquireLinkTimeOut;
-	private String gatewayAdress;
-	private boolean gatewayBound = false;
-	private String password;
-	private int port;
-	private byte priorityFlag;
-	private Properties properties = new Properties();
-	private byte protocolId;
-	private byte replaceIfPresentFlag;
-	private String serviceType;
-	private SMPPSession session = new SMPPSession();
-	private byte smDefaultMsgId;
-	private String sourceAddress;
-	private byte sourceAddressNPI;
-	private byte sourceAddressTON;
-	private String systemType;
-	private String userName;
 
 	public boolean bind() {
 
