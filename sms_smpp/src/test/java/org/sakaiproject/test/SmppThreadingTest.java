4a7d4d4786fed2c7832ae3ac69bca72700d3f821
Louis <louis@psybergate.co.za>
2008-11-21 10:32:27 +0000

    Smpp threading unit test

--- sms_smpp/src/test/java/org/sakaiproject/test/SmppThreadingTest.java
+++ sms_smpp/src/test/java/org/sakaiproject/test/SmppThreadingTest.java
@@ -6,71 +6,40 @@ import junit.framework.TestSuite;
 import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
 import net.sourceforge.groboutils.junit.v1.TestRunnable;
 
-import org.sakaiproject.sms.hibernate.model.SmsMessage;
-import org.sakaiproject.sms.impl.SmsSmppImpl;
-
+/**
+ * This unit test will create 2 or more separate but concurrent connections to
+ * the gateway simulator. Each thread will pass on messages. It will then hang
+ * on and wait for sms delivery reports to come in. The test will fail if
+ * delivery reports are missing. It will also fail if a message could not be
+ * sent to the gateway. The number of messages to be sent can be changed for
+ * each thread. The delay between message transmissions can also be set. The
+ * default is 100 ms.
+ * 
+ */
 public class SmppThreadingTest extends TestCase {
-	private class SmppSession extends TestRunnable {
-
-		private int delivery_count, success_count, batch_count;
-		private String sessionName;
-		private SmsSmppImpl smsSmppImpl;
-
-		private SmppSession(String sessionName, int batch_count) {
-			this.sessionName = sessionName;
-			smsSmppImpl = new SmsSmppImpl();
-			smsSmppImpl.init();
-			this.batch_count = batch_count;
-		}
-
-		@Override
-		public void runTest() throws Throwable {
-			System.out.println("Thread starting: " + sessionName);
-			for (int i = 0; i < batch_count; i++) {
-				SmsMessage smsMessage = new SmsMessage("+270731876135",
-						"Junit tesing forloop num:" + i);
-				System.out.println(sessionName + " send message " + i);
-				smsMessage.setId(i);
-
-				smsMessage = smsSmppImpl.sendMessageToGateway(smsMessage);
-				if (smsMessage.isSubmitResult()) {
-					success_count++;
-				}
-				Thread.sleep(100);
-			}
-			boolean waitForDeliveries = true;
-
-			// waiting for async delivery reports to arrive.
-			while (waitForDeliveries) {
-				System.out.println("Waiting for delivery reports");
-				delivery_count = smsSmppImpl.getDeliveryNotifications().size();
-				Thread.sleep(5000);
-				if (delivery_count == smsSmppImpl.getDeliveryNotifications()
-						.size()) {
-					waitForDeliveries = false;
-
-				}
-
-			}
-			System.out.println("Thread ended: " + sessionName);
-		}
-	}
+	private int session1_message_count = 50;
+	private int session2_message_count = 100;
+	private int delay_between_messages = 100; // ms
 
 	/**
-	 * Standard main() and suite() methods
+	 * Standard main() and suite() methods.
+	 * 
+	 * @param args
+	 *            the args
 	 */
 	public static void main(String[] args) {
 		String[] name = { SmppThreadingTest.class.getName() };
 		junit.textui.TestRunner.main(name);
 	}
 
+	/**
+	 * Suite.
+	 * 
+	 * @return the test
+	 */
 	public static Test suite() {
 		return new TestSuite(SmppThreadingTest.class);
 	}
-
-	private int session1_batch_count = 50;
-
-	private int session2_batch_count = 100;
 
 	/**
 	 * You use the MultiThreadedTestRunner in your test cases. The MTTR takes an
@@ -78,31 +47,38 @@ public class SmppThreadingTest extends TestCase {
 	 * 
 	 * After you have built the MTTR, you run it with a call to the
 	 * runTestRunnables() method.
+	 * 
+	 * @throws Throwable
+	 *             the throwable
 	 */
-	public void testExampleThread() throws Throwable {
+	public void testConcurrency() throws Throwable {
 
 		// instantiate the TestRunnable classes
-		SmppSession session1, session2;
+		SmppThread smsThread1, smsThread2;
 
-		session1 = new SmppSession("SMSC Session 1", session1_batch_count);
-		session2 = new SmppSession("SMSC Session 2", session2_batch_count);
+		smsThread1 = new SmppThread("SMSC Session 1", session1_message_count,
+				delay_between_messages);
+		smsThread2 = new SmppThread("SMSC Session 2", session2_message_count,
+				delay_between_messages);
 
 		// pass that instance to the MTTR
-		TestRunnable[] trs = { session1, session2 };
+		TestRunnable[] trs = { smsThread1, smsThread2 };
 		MultiThreadedTestRunner mttr = new MultiThreadedTestRunner(trs);
 
 		// kickstarts the MTTR & fires off threads
 		mttr.runTestRunnables();
+		int delivery_count = smsThread1.delivery_count
+				+ smsThread2.delivery_count;
+		int message_count = session1_message_count + session2_message_count;
+		// System.out.println(session1.sent_count);
+		// System.out.println(session2.sent_count);
+		System.out.println(delivery_count);
 
-		System.out.println(session1.success_count);
-		System.out.println(session2.success_count);
-		System.out.println(session1.delivery_count + session2.delivery_count);
-
-		assertEquals(true, session1.success_count == session1_batch_count);
-		assertEquals(true, session2.success_count == session2_batch_count);
-		assertEquals(
-				true,
-				(session1.delivery_count + session2.delivery_count) == (session1_batch_count + session2_batch_count));
+		assertEquals(true, smsThread1.sent_count == session1_message_count);
+		assertEquals(true, smsThread2.sent_count == session2_message_count);
+		// MOTE: smpp delivery reports will be sent to any of the report
+		// listeners. But eventually the number of reports must add up.
+		assertEquals(true, delivery_count == message_count);
 		System.out.println("Done");
 
 	}
