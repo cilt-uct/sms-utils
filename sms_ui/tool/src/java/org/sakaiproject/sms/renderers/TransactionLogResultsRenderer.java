2d4f03016decd5033b17ea13876e9dc6f95175a9
Julian <wilhelm@psybergate.co.za>
2008-12-04 08:00:10 +0000

    Changed the getSmsAccountId to getSmsAccount().getId() on the SmsTransaction object

--- sms_ui/tool/src/java/org/sakaiproject/sms/renderers/TransactionLogResultsRenderer.java
+++ sms_ui/tool/src/java/org/sakaiproject/sms/renderers/TransactionLogResultsRenderer.java
@@ -33,14 +33,15 @@ import uk.org.ponder.rsf.components.UIJointContainer;
 import uk.org.ponder.rsf.components.UIMessage;
 import uk.org.ponder.rsf.components.UIOutput;
 
-public class TransactionLogResultsRenderer implements SearchResultsRenderer{
+public class TransactionLogResultsRenderer implements SearchResultsRenderer {
 
-	private final static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(TransactionLogResultsRenderer.class);
-	
+	private final static org.apache.log4j.Logger LOG = org.apache.log4j.Logger
+			.getLogger(TransactionLogResultsRenderer.class);
+
 	private SearchFilterBean searchFilterBean;
 	private SortHeaderRenderer sortHeaderRenderer;
 	private SmsTransactionLogic smsTransactionLogic;
-	
+
 	public void setSmsTransactionLogic(SmsTransactionLogic smsTransactionLogic) {
 		this.smsTransactionLogic = smsTransactionLogic;
 	}
@@ -48,55 +49,79 @@ public class TransactionLogResultsRenderer implements SearchResultsRenderer{
 	public void setSearchFilterBean(SearchFilterBean searchFilterBean) {
 		this.searchFilterBean = searchFilterBean;
 	}
-	
-	public void init(){
+
+	public void init() {
 		sortHeaderRenderer = new SortHeaderRenderer();
 		Assert.notNull(smsTransactionLogic);
 	}
 
 	public void createTable(UIContainer tofill, String divID,
 			SortPagerViewParams sortViewParams, String viewID) {
-		
+
 		init();
-		
+
 		searchFilterBean.setOrderBy(sortViewParams.sortBy);
 		searchFilterBean.setSortDirection(sortViewParams.sortDir);
-		searchFilterBean.setCurrentPage(sortViewParams.current_start);		
-		
-		
+		searchFilterBean.setCurrentPage(sortViewParams.current_start);
+
 		List<SmsTransaction> smsTransactions = null;
 		boolean fail = false;
 		try {
-			smsTransactions = smsTransactionLogic.getSmsTransactionsForCriteria(searchFilterBean);
+			smsTransactions = smsTransactionLogic
+					.getSmsTransactionsForCriteria(searchFilterBean);
 		} catch (SmsSearchException e) {
 			LOG.error(e);
 			fail = true;
 		}
-		
-		UIJointContainer searchResultsTable = new UIJointContainer(tofill, divID,  "transaction-log-search-results-component:");
-		if(fail)
+
+		UIJointContainer searchResultsTable = new UIJointContainer(tofill,
+				divID, "transaction-log-search-results-component:");
+		if (fail)
 			UIMessage.make(searchResultsTable, "warning", "GeneralActionError");
-		else{
-			sortHeaderRenderer.makeSortingLink(searchResultsTable, "tableheader-account-number:", sortViewParams, "smsAccountId", "sms.transaction-log-search-results.account.no");
-			sortHeaderRenderer.makeSortingLink(searchResultsTable, "tableheader-transaction-type", sortViewParams, "transactionTypeCode", "sms.transaction-log-search-results.trans.type");
-			sortHeaderRenderer.makeSortingLink(searchResultsTable, "tableheader-transaction-date:", sortViewParams, "transactionDate", "sms.transaction-log-search-results.trans.date");
-			sortHeaderRenderer.makeSortingLink(searchResultsTable, "tableheader-transaction-credits:", sortViewParams, "transactionCredits", "sms.transaction-log-search-results.trans.credits");
-			sortHeaderRenderer.makeSortingLink(searchResultsTable, "tableheader-transaction-amount:", sortViewParams, "transactionAmount", "sms.transaction-log-search-results.trans.amount");
-			sortHeaderRenderer.makeSortingLink(searchResultsTable, "tableheader-account-balance:", sortViewParams, "balance", "sms.transaction-log-search-results.account.balanace");
+		else {
+			sortHeaderRenderer.makeSortingLink(searchResultsTable,
+					"tableheader-account-number:", sortViewParams,
+					"smsAccountId",
+					"sms.transaction-log-search-results.account.no");
+			sortHeaderRenderer.makeSortingLink(searchResultsTable,
+					"tableheader-transaction-type", sortViewParams,
+					"transactionTypeCode",
+					"sms.transaction-log-search-results.trans.type");
+			sortHeaderRenderer.makeSortingLink(searchResultsTable,
+					"tableheader-transaction-date:", sortViewParams,
+					"transactionDate",
+					"sms.transaction-log-search-results.trans.date");
+			sortHeaderRenderer.makeSortingLink(searchResultsTable,
+					"tableheader-transaction-credits:", sortViewParams,
+					"transactionCredits",
+					"sms.transaction-log-search-results.trans.credits");
+			sortHeaderRenderer.makeSortingLink(searchResultsTable,
+					"tableheader-transaction-amount:", sortViewParams,
+					"transactionAmount",
+					"sms.transaction-log-search-results.trans.amount");
+			sortHeaderRenderer.makeSortingLink(searchResultsTable,
+					"tableheader-account-balance:", sortViewParams, "balance",
+					"sms.transaction-log-search-results.account.balanace");
 
 			for (SmsTransaction smsTransaction : smsTransactions) {
 
-			UIBranchContainer row = UIBranchContainer.make(searchResultsTable, "dataset:");
+				UIBranchContainer row = UIBranchContainer.make(
+						searchResultsTable, "dataset:");
 
-			UIOutput.make(row, "row-data-account-number", NullHandling.safeToString(smsTransaction.getSmsAccountId()));
-			UIOutput.make(row, "row-data-transaction-type", smsTransaction.getTransactionTypeCode());
-			UIOutput.make(row, "row-data-transaction-date", NullHandling.safeToString(smsTransaction.getTransactionDate()));
-			UIOutput.make(row, "row-data-transaction-credits", NullHandling.safeToString(smsTransaction.getTransactionCredits()));
-			UIOutput.make(row, "row-data-transaction-amount", NullHandling.safeToString(smsTransaction.getTransactionAmount()));
-			UIOutput.make(row, "row-data-account-balance", NullHandling.safeToString(smsTransaction.getBalance()));
+				UIOutput.make(row, "row-data-account-number", NullHandling
+						.safeToString(smsTransaction.getSmsAccount().getId()));
+				UIOutput.make(row, "row-data-transaction-type", smsTransaction
+						.getTransactionTypeCode());
+				UIOutput.make(row, "row-data-transaction-date", NullHandling
+						.safeToString(smsTransaction.getTransactionDate()));
+				UIOutput.make(row, "row-data-transaction-credits", NullHandling
+						.safeToString(smsTransaction.getTransactionCredits()));
+				UIOutput.make(row, "row-data-transaction-amount", NullHandling
+						.safeToString(smsTransaction.getTransactionAmount()));
+				UIOutput.make(row, "row-data-account-balance", NullHandling
+						.safeToString(smsTransaction.getBalance()));
 			}
 		}
 	}
-
 
 }
