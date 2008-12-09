13349a987933331f9ce6e53c95640159dee2d847
Etienne <etienne@psybergate.co.za>
2008-12-09 06:34:06 +0000

    devlivery report changes. JIRA sms-2

--- sms_smpp/src/test/java/org/sakaiproject/test/SmppAPITest.java
+++ sms_smpp/src/test/java/org/sakaiproject/test/SmppAPITest.java
@@ -17,6 +17,7 @@
  **********************************************************************************/
 package org.sakaiproject.test;
 
+import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,33 +27,44 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import org.apache.log4j.Level;
+import org.sakaiproject.sms.hibernate.logic.impl.SmsMessageLogicImpl;
+import org.sakaiproject.sms.hibernate.logic.impl.SmsTaskLogicImpl;
 import org.sakaiproject.sms.hibernate.model.SmsMessage;
+import org.sakaiproject.sms.hibernate.model.SmsTask;
 import org.sakaiproject.sms.hibernate.model.constants.SmsConst_DeliveryStatus;
+import org.sakaiproject.sms.hibernate.model.constants.SmsConst_SmscDeliveryStatus;
 import org.sakaiproject.sms.impl.SmsSmppImpl;
 
 /**
  * Test some api function on the smpp api. For example successful connect and
- * disconnect to the remote gateway.
+ * disconnect to the remote gateway.Both multiple and single message sending is
+ * tested.
  */
 public class SmppAPITest extends TestCase {
 
 	private static SmsSmppImpl smsSmppImpl = null;
+	static SmsTask dummyTask = null;
+	private static SmsTaskLogicImpl smsTaskLogicImpl = null;
 
-	// The setup method instanciates the smsCoreImpl once. The teardown
-	// method safely calls disconnectGateWay at the end of the test
+	/**
+	 * The setup method instantiates the smsSmppImpl once. The tearDown method
+	 * safely calls disconnectGateWay at the end of the test
+	 */
+
 	public static Test suite() {
 
 		TestSetup setup = new TestSetup(new TestSuite(SmppAPITest.class)) {
 
 			protected void setUp() throws Exception {
-				System.out.println("setting up");
+				System.out.println("setUp");
 				smsSmppImpl = new SmsSmppImpl();
+				smsSmppImpl.setSmsMessageLogic(new SmsMessageLogicImpl());
 				smsSmppImpl.init();
 
 			}
 
 			protected void tearDown() throws Exception {
-				System.out.println("tear Down");
+				System.out.println("tearDown");
 				smsSmppImpl.disconnectGateWay();
 
 			}
@@ -61,8 +73,108 @@ public class SmppAPITest extends TestCase {
 		return setup;
 	}
 
-	// Testing the connect and disconnecting from the smsc. The test succeeds if
-	// the correct connectionStatus is returned.
+	/**
+	 * This is an helper method to insert a dummy smsTask into the Database. The
+	 * sakaiID is used to identify the temp task.
+	 */
+	public SmsTask insertNewTask(String sakaiID, String status,
+			Timestamp dateToSend, int attemptCount) {
+		SmsTask insertTask = new SmsTask();
+		insertTask.setSakaiSiteId(sakaiID);
+		insertTask.setSmsAccountId(0);
+		insertTask.setDateCreated(new Timestamp(System.currentTimeMillis()));
+		insertTask.setDateToSend(dateToSend);
+		insertTask.setStatusCode(status);
+		insertTask.setAttemptCount(0);
+		insertTask.setMessageBody("testing1234567");
+		insertTask.setSenderUserName("administrator");
+		smsTaskLogicImpl = new SmsTaskLogicImpl();
+		smsTaskLogicImpl.persistSmsTask(insertTask);
+		return insertTask;
+	}
+
+	/**
+	 * Test to send 10 smsMessages to the SMSC The test succeeds if the returned
+	 * status is STATUS_SENT and all the delivery reports are returned.
+	 */
+	public void testSendMessagesToGateway() {
+		smsSmppImpl.setLogLevel(Level.WARN);
+		Set<SmsMessage> smsMessages = new HashSet<SmsMessage>();
+
+		SmsTask insertTask = insertNewTask("testSendMessagesToGateway",
+				SmsConst_DeliveryStatus.STATUS_PENDING, new Timestamp(System
+						.currentTimeMillis()), 0);
+		assertTrue("Task for message not created", insertTask.exists());
+
+		for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			SmsMessage message = new SmsMessage();
+			message.setMobileNumber("0721998919");
+			message.setSakaiUserId("sakaiUserId");
+			message.setStatusCode(SmsConst_DeliveryStatus.STATUS_PENDING);
+			message.setSmsTask(insertTask);
+			smsMessages.add(message);
+
+		}
+
+		assertEquals(true, smsSmppImpl.sendMessagesToGateway(smsMessages)
+				.equals(SmsConst_DeliveryStatus.STATUS_SENT));
+
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, insertTask.getMessagesWithSmscStatus(
+				SmsConst_SmscDeliveryStatus.ENROUTE).size() == 0);
+		smsTaskLogicImpl.deleteSmsTask(insertTask);
+	}
+
+	/**
+	 * Test to send an single smsMessage to the SMSC.The test succeeds if an
+	 * SmscID is populated on the message and a delivery report is returned.
+	 */
+
+	public void testSendMessageToGateway() {
+		SmsTask insertTask = insertNewTask("testSendMessageToGateway",
+				SmsConst_DeliveryStatus.STATUS_PENDING, new Timestamp(System
+						.currentTimeMillis()), 0);
+
+		SmsMessage insertMessage1 = new SmsMessage();
+		insertMessage1.setMobileNumber("0721998919");
+		insertMessage1.setSakaiUserId("sakaiUserId");
+		insertMessage1.setStatusCode(SmsConst_DeliveryStatus.STATUS_PENDING);
+		insertMessage1.setSmsTask(insertTask);
+
+		assertEquals(true, smsSmppImpl.sendMessageToGateway(insertMessage1)
+				.getSmscMessageId() != null);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, insertTask.getMessagesWithSmscStatus(
+				SmsConst_SmscDeliveryStatus.ENROUTE).size() == 0);
+		smsTaskLogicImpl.deleteSmsTask(insertTask);
+
+	}
+
+	/**
+	 * The gateway return some information to us.
+	 */
+	public void testGetGatewayInfo() {
+		System.out.println(smsSmppImpl.getGatewayInfo());
+	}
+
+	/**
+	 * Testing the connect and disconnecting from the smsc. The test succeeds if
+	 * the correct connectionStatus is returned.
+	 */
+
 	public void testGetConnectionStatus() {
 
 		smsSmppImpl.disconnectGateWay();
@@ -73,64 +185,4 @@ public class SmppAPITest extends TestCase {
 
 	}
 
-	// Test to send 10 smsMessages to the SMSC and receive 11 devieryReports.
-	// The test succeeds if the returned status is
-	// STATUS_SENT and 10 devieryReports were received.
-	public void testSendMessagesToGateway() {
-		smsSmppImpl.setLogLevel(Level.WARN);
-		Set<SmsMessage> smsMessages = new HashSet<SmsMessage>();
-		for (int i = 0; i < 10; i++) {
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			SmsMessage smsMessage = new SmsMessage("+270731876135",
-					"Jnit tesing forloop num:" + i);
-			smsMessage.setId(new Long(i));
-			smsMessages.add(smsMessage);
-
-		}
-		smsSmppImpl.getDeliveryNotifications().clear();
-		assertEquals(true, smsSmppImpl.sendMessagesToGateway(smsMessages)
-				.equals(SmsConst_DeliveryStatus.STATUS_SENT));
-		waitForDeliveries();
-
-		assertEquals(true, smsSmppImpl.getDeliveryNotifications().size() == 10);
-		smsSmppImpl.getDeliveryNotifications().clear();
-	}
-
-	public void waitForDeliveries() {
-		boolean waitForDeliveries = true;
-		while (waitForDeliveries) {
-			int reportsReceived = smsSmppImpl.getDeliveryNotifications().size();
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			int delivery_count = smsSmppImpl.getDeliveryNotifications().size();
-			if (delivery_count == reportsReceived) {
-				delivery_count = smsSmppImpl.getDeliveryNotifications().size();
-				waitForDeliveries = false;
-
-			}
-		}
-	}
-
-	// Test to send an single smsMessage to the SMSC.The test succeeds if an
-	// SmscID is poputated on the message.
-	public void testSendMessageToGateway() {
-		SmsMessage smsMessage = new SmsMessage("+270731876135", "testing12345");
-		assertEquals(true, smsSmppImpl.sendMessageToGateway(smsMessage)
-				.getSmscMessageId() != null);
-		waitForDeliveries();
-		assertEquals(true, smsSmppImpl.getDeliveryNotifications().size() == 1);
-
-	}
-
-	// The gateway return some information to us.
-	public void testGetGatewayInfo() {
-		System.out.println(smsSmppImpl.getGatewayInfo());
-	}
 }
