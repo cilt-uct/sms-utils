8b2e2308121523ae47fbbb2be060cd05d0af01da
Etienne <etienne@psybergate.co.za>
2008-12-12 07:34:08 +0000

    modified smstask with updated fields sms:2

--- sms_smpp/src/test/java/org/sakaiproject/sms/test/SmppAPITest.java
+++ sms_smpp/src/test/java/org/sakaiproject/sms/test/SmppAPITest.java
@@ -1,17 +1,17 @@
 /***********************************************************************************
  * SmppAPITest.java
  * Copyright (c) 2008 Sakai Project/Sakai Foundation
- * 
- * Licensed under the Educational Community License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.osedu.org/licenses/ECL-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.s
  *
  **********************************************************************************/
@@ -87,6 +87,8 @@ public class SmppAPITest extends AbstractBaseTestCase {
 		insertTask.setAttemptCount(0);
 		insertTask.setMessageBody("testing1234567");
 		insertTask.setSenderUserName("administrator");
+		insertTask.setMaxTimeToLive(1000);
+		insertTask.setDelReportTimeoutDuration(1000);
 		smsTaskLogicImpl = new SmsTaskLogicImpl();
 		smsTaskLogicImpl.persistSmsTask(insertTask);
 		return insertTask;
