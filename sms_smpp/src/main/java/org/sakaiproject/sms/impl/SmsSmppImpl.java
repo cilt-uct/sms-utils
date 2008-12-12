9a7a402ec4ea9efdcde1e36135e90cdb330d843a
Etienne <etienne@psybergate.co.za>
2008-12-12 07:48:59 +0000

    added sending delay property sms:2

--- sms_smpp/src/main/java/org/sakaiproject/sms/impl/SmsSmppImpl.java
+++ sms_smpp/src/main/java/org/sakaiproject/sms/impl/SmsSmppImpl.java
@@ -1,17 +1,17 @@
 /***********************************************************************************
  * SmsSmppImpl.java
  * Copyright (c) 2008 Sakai Project/Sakai Foundation
- * 
- * Licensed under the Educational Community License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.osedu.org/licenses/ECL-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  **********************************************************************************/
@@ -99,7 +99,8 @@ public class SmsSmppImpl implements SmsSmpp {
 	private String userName;
 	private String addressRange;
 	public SmsMessageLogic smsMessageLogic = null;
-	public int transactionTimer;
+	private int transactionTimer;
+	private int sendingDelay;
 
 	public SmsMessageLogic getSmsMessageLogic() {
 		return smsMessageLogic;
@@ -389,6 +390,9 @@ public class SmsSmppImpl implements SmsSmpp {
 			transactionTimer = Integer.parseInt(properties
 					.getProperty("transactionTimer")) * 1000;
 
+			sendingDelay = Integer.parseInt(properties
+					.getProperty("sendingDelay"));
+
 		} catch (Exception e) {
 			LOG.error("Properies faild to load" + e);
 		}
@@ -498,6 +502,12 @@ public class SmsSmppImpl implements SmsSmpp {
 			if (!gatewayBound) {
 				return (SmsConst_DeliveryStatus.STATUS_INCOMPLETE);
 
+			}
+			try {
+				Thread.sleep(sendingDelay);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			message = sendMessageToGateway(message);
 			if (!message.getStatusCode().equals(
