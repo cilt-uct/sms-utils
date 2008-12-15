d405c2cc284ce8e858b31f0ef866a6b307b5664e
Etienne <etienne@psybergate.co.za>
2008-12-15 12:06:30 +0000

    changed hibernate mappings file. sms:3

--- sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/model/SmsAccount.java
+++ sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/model/SmsAccount.java
@@ -1,23 +1,24 @@
 /***********************************************************************************
  * SmsAccount.java
  * Copyright (c) 2008 Sakai Project/Sakai Foundation
- * 
- * Licensed under the Educational Community License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.osedu.org/licenses/ECL-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  **********************************************************************************/
 
 package org.sakaiproject.sms.hibernate.model;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -55,10 +56,31 @@ public class SmsAccount extends BaseModel {
 	/** The account name. */
 	private String accountName;
 
+	private Date startdate;
+
+	private Date enddate;
+
 	/**
 	 * Instantiates a new sms account.
 	 */
 	public SmsAccount() {
+		this.startdate = new Date(System.currentTimeMillis());
+	}
+
+	public Date getStartdate() {
+		return startdate;
+	}
+
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+
+	public Date getEnddate() {
+		return enddate;
+	}
+
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
 	}
 
 	/**
