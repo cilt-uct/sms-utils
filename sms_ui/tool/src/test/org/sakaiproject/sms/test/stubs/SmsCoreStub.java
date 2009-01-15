e98a73a28f6aa36978252be80a57e97114210ab6
wilhelm <wilhelm@psybergate.co.za>
2009-01-15 07:35:41 +0000

    SMS-3 Unit tests for HelperActionBean

--- sms_ui/tool/src/test/org/sakaiproject/sms/test/stubs/SmsCoreStub.java
+++ sms_ui/tool/src/test/org/sakaiproject/sms/test/stubs/SmsCoreStub.java
@@ -1,3 +1,20 @@
+/***********************************************************************************
+ * SmsCoreStub.java
+ * Copyright (c) 2008 Sakai Project/Sakai Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 package org.sakaiproject.sms.test.stubs;
 
 import java.util.Date;
@@ -8,6 +25,8 @@ import org.sakaiproject.sms.hibernate.model.SmsMessage;
 import org.sakaiproject.sms.hibernate.model.SmsTask;
 
 public class SmsCoreStub implements SmsCore {
+
+	public boolean insertTaskCalled = false;
 
 	public SmsTask calculateEstimatedGroupSize(SmsTask arg0) {
 		// TODO Auto-generated method stub
@@ -40,8 +59,8 @@ public class SmsCoreStub implements SmsCore {
 	}
 
 	public SmsTask insertTask(SmsTask arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		insertTaskCalled = true;
+		return arg0;
 	}
 
 	public void processIncomingMessage(SmsMessage arg0) {
--- sms_ui/tool/src/test/org/sakaiproject/sms/test/stubs/SmsServiceStub.java
+++ sms_ui/tool/src/test/org/sakaiproject/sms/test/stubs/SmsServiceStub.java
@@ -1,3 +1,20 @@
+/***********************************************************************************
+ * SmsServiceStub.java
+ * Copyright (c) 2008 Sakai Project/Sakai Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 package org.sakaiproject.sms.test.stubs;
 
 import java.util.ArrayList;
@@ -9,23 +26,29 @@ import org.sakaiproject.sms.hibernate.model.SmsTask;
 
 public class SmsServiceStub implements SmsService {
 
+	public boolean calculateCalled = false;
+	public boolean sufficientCredits = false;
+
+	/**
+	 * Sets boolean to check if method called
+	 */
 	public SmsTask calculateEstimatedGroupSize(SmsTask arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		arg0.setCreditEstimate(2);
+		calculateCalled = true;
+		return arg0;
 	}
 
 	public boolean checkSufficientCredits(String arg0, String arg1, int arg2) {
-		// TODO Auto-generated method stub
-		return false;
+		return sufficientCredits;
 	}
 
-	public SmsTask getPreliminaryTask(String arg0, Date arg1, String arg2,
+	public SmsTask getPreliminaryTask(Set<String> arg0, Date arg1, String arg2,
 			String arg3, String arg4, String arg5) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public SmsTask getPreliminaryTask(Set<String> arg0, Date arg1, String arg2,
+	public SmsTask getPreliminaryTask(String arg0, Date arg1, String arg2,
 			String arg3, String arg4, String arg5) {
 		// TODO Auto-generated method stub
 		return null;
