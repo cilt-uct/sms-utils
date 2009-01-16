0f3ea7bafcd68abbd6676fa4c65b860058bba1a1
Grant <grant@psybergate.co.za>
2009-01-16 16:41:45 +0000

    SMS-3 Validation tests.

---
+++ sms_ui/tool/src/java/org/sakaiproject/sms/validators/SmsSystemConfigValidator.java
@@ -0,0 +1,24 @@
+package org.sakaiproject.sms.validators;
+
+import org.sakaiproject.sms.hibernate.model.SmsConfig;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class SmsSystemConfigValidator implements Validator{
+
+	@SuppressWarnings("unchecked")
+	public boolean supports(Class clazz) {
+		if (SmsConfig.class.equals(clazz.getClass())) {
+			return true;
+		}
+		return false;
+	}
+
+	public void validate(Object target, Errors errors) {
+				
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "delReportTimeoutDuration", "sms.errors.gateway.empty");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors,"schedulerInterval", "sms.errors.scheduler.interval");
+	}
+
+}
---
+++ sms_ui/tool/src/test/org/sakaiproject/sms/test/SmsSystemConfigValidatorTest.java
@@ -0,0 +1,37 @@
+package org.sakaiproject.sms.test;
+
+import junit.framework.TestCase;
+
+import org.sakaiproject.sms.hibernate.model.SmsConfig;
+import org.sakaiproject.sms.validators.SmsSystemConfigValidator;
+import org.springframework.validation.BindException;
+
+public class SmsSystemConfigValidatorTest extends TestCase{
+
+	private SmsSystemConfigValidator validator;
+	private BindException bindException;
+	private SmsConfig smsConfig;
+
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		smsConfig = new SmsConfig();
+		validator = new SmsSystemConfigValidator();
+		bindException = new BindException(smsConfig, "SmsConfig");
+	}
+
+	public void testInvalidConfigSettings() throws Exception {
+		smsConfig.setDelReportTimeoutDuration(null);
+		smsConfig.setSchedulerInterval(null);
+		validator.validate(smsConfig, bindException);
+		assertTrue(bindException.hasErrors());
+	}
+	
+	public void testValidConfigSettings() throws Exception {
+		smsConfig.setDelReportTimeoutDuration(new Integer(100));
+		smsConfig.setSchedulerInterval(new Integer(100));
+		validator.validate(smsConfig, bindException);
+		assertFalse(bindException.hasErrors());
+	}
+}
