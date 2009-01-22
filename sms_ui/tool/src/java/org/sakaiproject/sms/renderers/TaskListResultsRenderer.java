d4b366473c11b7e43eb1afc58b334c55c43e142e
Grant <grant@psybergate.co.za>
2009-01-22 09:23:56 +0000

    SMS-3 Date Processed

--- sms_ui/tool/src/java/org/sakaiproject/sms/renderers/TaskListResultsRenderer.java
+++ sms_ui/tool/src/java/org/sakaiproject/sms/renderers/TaskListResultsRenderer.java
@@ -144,7 +144,7 @@ public class TaskListResultsRenderer implements SearchResultsRenderer {
 				UIOutput
 						.make(row, "row-data-message", NullHandling.safeTruncatedToString(smsTask.getMessageBody(), 20));
 				UIOutput.make(row, "row-data-process-date", NullHandling
-						.safeToStringFormated(smsTask.getDateToSend()));
+						.safeToStringFormated(smsTask.getDateProcessed()));
 				UIOutput.make(row, "row-data-status", NullHandling.safeToString(smsTask.getStatusCode()));
 				
 				id++;
