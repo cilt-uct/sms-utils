c09d3ce304b848c6bf86fb5e8c29f819869a9c33
Julian <wilhelm@psybergate.co.za>
2009-01-22 06:41:45 +0000

    SMS-4 Added  constant for transaction debit account: TRANS_DEBIT_ACCOUNT

--- sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/model/constants/SmsConst_Billing.java
+++ sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/model/constants/SmsConst_Billing.java
@@ -13,7 +13,9 @@ public class SmsConst_Billing {
 	public static final String TRANS_SETTLE_DIFFERENCE = "RSET";
 
 	public static final String TRANS_CANCEL_RESERVE = "RCAN";
-	
+
 	public static final String TRANS_CANCEL = "TCAN";
+
+	public static final String TRANS_DEBIT_ACCOUNT = "DEB";
 
 }
