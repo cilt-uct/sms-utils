39034a082ff9511200b16e79aa9f2b0f796bb610
Louis <louis@psybergate.co.za>
2009-01-26 05:38:25 +0000

    SMS-4: Make createCancelTask public

--- sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/model/factory/SmsTransactionFactory.java
+++ sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/model/factory/SmsTransactionFactory.java
@@ -8,49 +8,55 @@ import org.sakaiproject.sms.hibernate.model.constants.SmsConst_Billing;
 
 public class SmsTransactionFactory {
 
-	public static SmsTransaction createReserveCreditsTask(Long smsTaskId, Long smsAccountId, Integer credits) throws SmsAccountNotFoundException{
-		
+	// Not in use, but there are unit test for it
+	private static SmsTransaction createReserveCreditsTask(Long smsTaskId,
+			Long smsAccountId, Integer credits)
+			throws SmsAccountNotFoundException {
+
 		SmsAccount smsAccount = getSmsAccount(smsAccountId);
-		
+
 		SmsTransaction smsTransaction = new SmsTransaction();
 		smsTransaction.setSmsAccount(smsAccount);
 		smsTransaction.setSakaiUserId(smsAccount.getSakaiUserId());
 		smsTransaction.setTransactionCredits(credits);
-		smsTransaction.setTransactionTypeCode(SmsConst_Billing.TRANS_RESERVE_CREDITS);
+		smsTransaction
+				.setTransactionTypeCode(SmsConst_Billing.TRANS_RESERVE_CREDITS);
 		smsTransaction.setSmsTaskId(smsTaskId);
 		smsTransaction.setBalance(smsAccount.getBalance());
-		
-		//TODO : Check to see if this is the case
+
+		// TODO : Check to see if this is the case
 		smsTransaction.setTransactionAmount(0.0f);
-		
+
 		return smsTransaction;
 	}
 
-
-	public static SmsTransaction createCancelTask(Long smsTaskId, Long smsAccountId) throws SmsAccountNotFoundException {
+	// Not in use, but there are unit test for it
+	public static SmsTransaction createCancelTask(Long smsTaskId,
+			Long smsAccountId) throws SmsAccountNotFoundException {
 		SmsAccount smsAccount = getSmsAccount(smsAccountId);
-		
+
 		SmsTransaction smsTransaction = new SmsTransaction();
 		smsTransaction.setSmsAccount(smsAccount);
 		smsTransaction.setSakaiUserId(smsAccount.getSakaiUserId());
 		smsTransaction.setTransactionTypeCode(SmsConst_Billing.TRANS_CANCEL);
 		smsTransaction.setSmsTaskId(smsTaskId);
-		
+
 		smsTransaction.setTransactionCredits(0);
 		smsTransaction.setTransactionAmount(0.0f);
 		smsTransaction.setBalance(0.0f);
-		
+
 		return smsTransaction;
 	}
 
 	private static SmsAccount getSmsAccount(Long smsAccountId)
-	throws SmsAccountNotFoundException {
-		SmsAccount smsAccount = HibernateLogicFactory.getAccountLogic().getSmsAccount(smsAccountId);
-		
-		if(smsAccount == null)
-			throw new SmsAccountNotFoundException("Account id " + smsAccountId + " does not exsits");
+			throws SmsAccountNotFoundException {
+		SmsAccount smsAccount = HibernateLogicFactory.getAccountLogic()
+				.getSmsAccount(smsAccountId);
+
+		if (smsAccount == null)
+			throw new SmsAccountNotFoundException("Account id " + smsAccountId
+					+ " does not exsits");
 		return smsAccount;
 	}
 
-	
 }
