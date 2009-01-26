47a31a1f6da4130d852dc6c73eda48c7dc7caf90
Etienne <etienne@psybergate.co.za>
2009-01-26 08:01:31 +0000

    sms:4 fixed incorrect query

--- sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/logic/impl/SmsAccountLogicImpl.java
+++ sms_hibernate/src/main/java/org/sakaiproject/sms/hibernate/logic/impl/SmsAccountLogicImpl.java
@@ -1,17 +1,17 @@
 /***********************************************************************************
  * SmsAccountLogicImpl.java
  * Copyright (c) 2008 Sakai Project/Sakai Foundation
- * 
- * Licensed under the Educational Community License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.osedu.org/licenses/ECL-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  **********************************************************************************/
@@ -160,8 +160,8 @@ public class SmsAccountLogicImpl extends SmsDao implements SmsAccountLogic {
 		SmsAccount account = null;
 		StringBuilder hql = new StringBuilder();
 		hql
-				.append(" from SmsAccount account where account.enddate is null or account.enddate > :today ");
-		hql.append(" and account.sakaiSiteId = :sakaiSiteId");
+				.append(" from SmsAccount account where (account.enddate is null or account.enddate > :today) ");
+		hql.append(" and (account.sakaiSiteId = :sakaiSiteId)");
 		List<SmsAccount> accounts = new ArrayList<SmsAccount>();
 		Session s = HibernateUtil.getSession();
 		Query query = s.createQuery(hql.toString());
